29a774c105a2d8d2d9f8a36bda81fa5318542fd7
DJCubed12 <djcubed12@gmail.com>
2023-06-21 21:34:06 -0500

    Minimal setup work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppComponent } from './app.component';
 import { EntryComponent } from './entry/entry.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EntryComponent
-  ],
+  declarations: [AppComponent, EntryComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+const URL = 'http://localhost:3000/test';
+
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.css']
+  styleUrls: ['./entry.component.css'],
 })
 export class EntryComponent {
+  public data: { value: string } | null = null;
 
+  async getData(): Promise<void> {
+    const data = await fetch(URL);
+    this.data = (await data.json()) ?? null;
+  }
 }
