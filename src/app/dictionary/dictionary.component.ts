73e4fe5a2e104a698c9c8e0216ffccab6a37a5f3
DJCubed12 <djcubed12@gmail.com>
2023-06-27 20:40:22 -0500

    Basic translate functionality

--- src/app/dictionary/dictionary.component.ts
+++ src/app/dictionary/dictionary.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 
 import sdapi from 'sdapi';
+import { WordResult } from 'sdapi/lib/dictionary';
 
 @Component({
   selector: 'dictionary',
@@ -11,4 +12,18 @@ import sdapi from 'sdapi';
   standalone: true,
   imports: [MatInputModule],
 })
-export class DictionaryComponent {}
+export class DictionaryComponent {
+  public results: string = '';
+
+  public translate(word: string): void {
+    this.results = 'Searching...';
+    sdapi.translate(word).then(
+      (results) => this.showResults(results),
+      (err) => console.log('Translate error: is CORS disabled?')
+    );
+  }
+
+  private showResults(results: WordResult[]): void {
+    this.results = JSON.stringify(results);
+  }
+}
