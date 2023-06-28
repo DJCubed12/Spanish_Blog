b07b23fd72bd7ffd9b90673e1bf81ebdfdb15578
DJCubed12 <djcubed12@gmail.com>
2023-06-27 21:07:03 -0500

    Dictionary passes WordResult to Result component

--- src/app/dictionary/result/result.component.ts
+++ src/app/dictionary/result/result.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common'; // For ngIf, ngFor...
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // For ngIf, ngFor...
+
+import { WordResult } from 'sdapi/lib/dictionary';
 
 @Component({
   selector: 'result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css'],
   standalone: true,
+  imports: [CommonModule],
 })
-export class ResultComponent {}
+export class ResultComponent {
+  @Input() result: WordResult | null = null;
+}
