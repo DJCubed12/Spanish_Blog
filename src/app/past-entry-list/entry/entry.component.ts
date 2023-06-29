6b8aa805715e780da6f37faa830f822e9841d57d
DJCubed12 <djcubed12@gmail.com>
2023-06-28 21:51:51 -0500

    Past entries displayed in EntryComponent in MatCard

--- src/app/past-entry-list/entry/entry.component.ts
+++ src/app/past-entry-list/entry/entry.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+
+import { Entry } from 'src/models';
 
 @Component({
   selector: 'entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
 })
-export class EntryComponent {}
+export class EntryComponent {
+  @Input() entry: Entry | null = null;
+}
