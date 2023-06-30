dc8f2722feccad62b8e56edd6a41d89875600ed6
DJCubed12 <djcubed12@gmail.com>
2023-06-29 21:25:32 -0500

    Glossary on past entries

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { Entry } from 'src/models';
 
@@ -10,8 +12,13 @@ import { Entry } from 'src/models';
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css'],
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatChipsModule, MatTooltipModule],
 })
 export class EntryComponent {
   @Input() entry?: Entry;
+
+  clickedWord(glossaryIndex: number) {
+    // NOTE: Clicking remove icon doesn't trigger this; Very good for my purposes of changing its color and showing definition
+    console.log(`${this.entry!.glossary[glossaryIndex].word} was clicked.`);
+  }
 }
